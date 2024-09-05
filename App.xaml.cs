19b31cb99fc1061532fd63ebeb2bae7cdfaba82f
CyberAustin <88937794+CyberAustin@users.noreply.github.com>
2024-09-05 07:47:19 +0000

    Removed OneSignal because it was sending telemitry data back on top of handling Push Notifications.

--- App.xaml.cs
+++ App.xaml.cs
@@ -10,16 +10,6 @@ namespace SOFAGasBuddy
         public App()
         {
             InitializeComponent();
-            // Enable verbose OneSignal logging to debug issues if needed.
-            OneSignal.Debug.LogLevel = LogLevel.ERROR;
-
-            // OneSignal Initialization
-            OneSignal.Initialize("3824bae2-5cab-437b-9c86-a0b8f4033bcf");
-
-            // RequestPermissionAsync will show the notification permission prompt.
-            // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 5)
-            OneSignal.Notifications.RequestPermissionAsync(true);
-
             MainPage = new AppShell();
         }
     }
